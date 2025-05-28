768d5c2853c36cd98e4d40079e6a6935bde61984
Lukas Matena <lukasmatena@seznam.cz>
2025-05-28 22:00:23 +0200

    Improved connection to database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,52 +1,32 @@
-from fastapi import FastAPI
-import asyncio
-import asyncpg
-from os import environ
+from fastapi import FastAPI, Depends, HTTPException
+from database import db_connection_pool_init, db_get_connection
 
-app = FastAPI()
 
-pg_user = environ.get("POSTGRES_USER")
-pg_pass = environ.get("POSTGRES_PASSWORD")
-pg_host = environ.get("POSTGRES_HOST")
-pg_port = environ.get("POSTGRES_PORT")
-pg_db   = environ.get("POSTGRES_DB")
+app = FastAPI(lifespan=db_connection_pool_init)
+
 
 @app.get("/")
 async def hello_world():
     return "Hello from Python backend!"
 
 @app.get("/test_db/")
-async def test_db():
-    db_url: str = f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
+async def test_db(conn = Depends(db_get_connection)):    
     try:
-        conn = await asyncpg.connect(dsn=db_url, timeout=5)
-    except asyncpg.exceptions.InvalidPasswordError:
-        print(f"Connection failed: Invalid password for user '{pg_user}'")
-        return "NOK - Invalid password"
-    except asyncpg.exceptions.CannotConnectNowError as e:
-        print(f"Connection failed: Cannot connect now (e.g., server down, wrong host/port). Error: {e}")
-        return "NOK - Cannot connect to server"
-    except ConnectionRefusedError as e:
-        print(f"Connection failed: Connection refused by server. Error: {e}")
-        return "NOK - Connection refused"
-    except TimeoutError:
-        print(f"Connection failed: Connection attempt timed out.")
-        return "NOK - Connection timeout"
+        async with conn.transaction(readonly = True):
+            return await conn.fetch("SELECT * FROM trips;")
     except Exception as e:
-        print(f"Connection failed: An unexpected error occurred: {type(e).__name__} - {e}")
-        return f"NOK - An unexpected error occurred: {type(e).__name__}"
-    #finally:
-    #    if conn:
-    #        await conn.close()
+        raise HTTPException(status_code=503, detail = f"Error occurred: {type(e).__name__}")
 
+@app.get("/reset_db/")
+async def reset_db(conn = Depends(db_get_connection)):
     try:
-        #await conn.execute("DROP TABLE IF EXISTS trips")
-        #await conn.execute("CREATE TABLE trips ( id NUMERIC PRIMARY KEY, title VARCHAR(255) );")
-        #await conn.execute("INSERT INTO trips VALUES (2, 'druhy');")
-        return await conn.fetch("SELECT * FROM trips;")
+        async with conn.transaction(readonly = False):
+            await conn.execute("""
+                               DROP TABLE IF EXISTS trips;
+                               CREATE TABLE trips (id SERIAL PRIMARY KEY, title VARCHAR(255), description TEXT);
+                               INSERT INTO trips (title, description) VALUES ('titulek1', 'text1');
+                               INSERT INTO trips (title, description) VALUES ('titulek2', 'text2');
+                               INSERT INTO trips (title, description) VALUES ('titulek3', 'text3');
+                               """)
     except Exception as e:
-        return f"NOK - An unexpected error occurred: {type(e).__name__}"
-    if conn:
-        await conn.close()
-
-    return "OK"
+        raise HTTPException(status_code=503)
