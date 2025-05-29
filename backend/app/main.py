ee0087bf017fc826326ead3639b6f4502dd52dd3
Lukas Matena <lukasmatena@seznam.cz>
2025-05-29 15:14:36 +0200

    Created photos table and some endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,33 @@
 from fastapi import FastAPI, Depends, HTTPException
 from database import db_connection_pool_init, db_get_connection
+import logging
+import sys
 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
 app = FastAPI(lifespan=db_connection_pool_init)
+
 
 
 @app.get("/")
 async def hello_world():
     return "Hello from Python backend!"
 
-@app.get("/test_db/")
-async def test_db(conn = Depends(db_get_connection)):    
+@app.get("/read_db/")
+async def read_db(conn = Depends(db_get_connection)):    
     try:
         async with conn.transaction(readonly = True):
-            return await conn.fetch("SELECT * FROM trips;")
+            trips = await conn.fetch("SELECT * FROM trips;")
+            photos = await conn.fetch("SELECT * FROM photos;")
+            return {"trips_table": list(trips), "photos_table": list(photos)}
     except Exception as e:
         raise HTTPException(status_code=503, detail = f"Error occurred: {type(e).__name__}")
 
@@ -22,11 +36,52 @@ async def reset_db(conn = Depends(db_get_connection)):
     try:
         async with conn.transaction(readonly = False):
             await conn.execute("""
-                               DROP TABLE IF EXISTS trips;
-                               CREATE TABLE trips (id SERIAL PRIMARY KEY, title VARCHAR(255), description TEXT);
-                               INSERT INTO trips (title, description) VALUES ('titulek1', 'text1');
-                               INSERT INTO trips (title, description) VALUES ('titulek2', 'text2');
-                               INSERT INTO trips (title, description) VALUES ('titulek3', 'text3');
-                               """)
+                DROP TABLE IF EXISTS photos;
+                DROP TABLE IF EXISTS trips;
+                CREATE TABLE trips (
+                    trip_id SERIAL PRIMARY KEY,
+                    title VARCHAR(255),
+                    description TEXT);
+                CREATE TABLE photos (
+                    photo_id SERIAL PRIMARY KEY,
+                    trip_id INT,
+                    CONSTRAINT fk_trips_photos
+                        FOREIGN KEY (trip_id)
+                        REFERENCES trips(trip_id)
+                        ON DELETE SET NULL
+                );
+                """)
+            await conn.execute("""
+                INSERT INTO trips (title, description) VALUES ('titulek1', 'text1');
+                INSERT INTO trips (title, description) VALUES ('titulek2', 'text2');
+                INSERT INTO trips (title, description) VALUES ('titulek3', 'text3');
+            """)
+            await conn.execute("""
+                INSERT INTO photos (trip_id) VALUES (1);
+            """)
     except Exception as e:
-        raise HTTPException(status_code=503)
+        logging.error(f"Failed to reset database: {e}", exc_info=True)
+        raise HTTPException(status_code=503, detail = f"{type(e).__name__}")
+    return "OK"
+
+
+
+@app.get("/trips/{trip_id}")
+async def get_trip(trip_id: int, conn = Depends(db_get_connection)):
+    try:
+        async with conn.transaction(readonly = True):
+            query = f"SELECT * FROM trips WHERE trip_id=$1"
+            trip_data = await conn.fetchrow(query, trip_id)
+            query = "SELECT photos.photo_id FROM photos WHERE trip_id=$1;"
+            photos_list = await conn.fetch(query, trip_id)
+            if not trip_data:
+                logging.error(f"Trip {trip_id} not found.")
+                raise HTTPException(status_code=404, detail=f"Trip {trip_id} not found.")
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        logging.error(f"Unable to retrieve trip from db: {e}", exc_info=True)
+        raise HTTPException(status_code=503, detail=f"Error: {type(e).__name__}")
+    out = dict(trip_data)
+    out["photos_table"] = photos_list
+    return out
