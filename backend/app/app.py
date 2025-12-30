b21d4fc770cdc79ad465829f637914bad603c759
Lukas Matena <lukasmatena@seznam.cz>
2025-12-30 11:22:44 +0100

    Backend deployment + related changes

--- backend/app/app.py
+++ backend/app/app.py
@@ -30,28 +30,41 @@ async def app_init(app_inst: FastAPI):
     pg_host = environ.get("POSTGRES_HOST")
     pg_port = environ.get("POSTGRES_PORT")
     pg_db   = environ.get("POSTGRES_DB")
-    db_url: str = f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
+    db_url: str = ""
+    if pg_host.startswith("/"):
+        # Cloud Run (Unix Socket) - Host goes in query params, NO PORT in authority
+        db_url = f"postgresql://{pg_user}:{pg_pass}@/{pg_db}?host={pg_host}"
+    else:
+        # Local/TCP - Standard Host:Port format
+        db_url = f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
     try:
         pool = await asyncpg.create_pool(dsn = db_url, timeout = 5)
     except Exception as e:
-        logging.error(f"Unable to connect to the db: {db_url}")
+        logging.error(f"Unable to connect to the db.")
         raise e
     logging.info(" - db connection pool created.")
 
     # Initialize GCP Storage handle
     gcp_storage_client = google.cloud.storage.Client()
     logging.info(" - GCP Storage client created")
+    
+    bucket_name: str | None = environ.get("GCP_STORAGE_BUCKET_NAME")
+    if not bucket_name:
+        raise Exception("ERROR: unknown GCP Storage bucket name")
+
+    bucket = gcp_storage_client.bucket(bucket_name) # no network call yet
     try:
-        bucket_name: str | None = environ.get("GCP_STORAGE_BUCKET_NAME")
-        if not bucket_name:
-            raise Exception("ERROR: unknown GCP Storage bucket name")
-        bucket = gcp_storage_client.create_bucket(bucket_name, timeout=10)
-        logging.info(f" - GCP Storage bucket created ({bucket_name})")
+        # This is for local devlopment, where we need to ensure the bucket exists.
+        bucket.create(location="europe-west1")
+        logging.info(f" - Created new bucket: {bucket_name}")
     except google.cloud.exceptions.Conflict:
-        logging.info(f" - GCP Storage bucket already exists ({bucket_name})")
-        bucket = gcp_storage_client.get_bucket(bucket_name)
+        logging.info(f" - Bucket {bucket_name} already exists (Local)")
+    except google.cloud.exceptions.Forbidden:
+        # Production: We are not allowed to create buckets.
+        # We assume Terraform already created it and proceed.
+        logging.info(f" - explicit creation denied for {bucket_name}, assuming infrastructure exists (Production)")
     except Exception as e:
-        logging.error(f" - Unable to create GCP Storage bucket ({bucket_name})")
+        logging.error(f" - Unexpected error connecting to bucket {bucket_name}")
         raise e
     if not bucket:
         raise Exception(f"ERROR: GCP Storage bucket not available ({bucket_name})")
---
+++ backend/deploy.sh
@@ -0,0 +1,46 @@
+# Running migrations
+#
+# Migrations are run from local environment using SQL proxying. In one terminal, run
+#
+# $ cloud-sql-proxy.exe --port 5433 --quota-project hiking-trips-stage hiking-trips-stage:europe-west1:trips-db
+#
+# In a different terminal, cd to backend folder, set up Python venv, install alembic and then
+#
+# $ export POSTGRES_DB=trips_db
+# $ export POSTGRES_HOST=127.0.0.1
+# $ export POSTGRES_PORT=5433
+# $ export POSTGRES_USER=(see console)
+# $ export POSTGRES_PASSWORD=(see secrets)
+# $ venv/Scripts/alembic upgrade head
+
+
+# The actual deploy script follows:
+
+
+if [ "$1" != "stage" ] && [ "$1" != "prod" ]; then
+  echo "Usage: ./deploy.sh [stage|prod]"
+  exit 1
+fi
+
+# These must match Terraform config:
+REGION="europe-west1"
+PROJECT_ID="hiking-trips-$1"
+REPO="backend-repo"
+
+# Other vars
+IMAGE="trips-backend"
+TAG=$(git rev-parse --short HEAD)
+
+IMAGE_URL="$REGION-docker.pkg.dev/$PROJECT_ID/$REPO/$IMAGE:$TAG"
+
+echo "Deploying $TAG to $1..."
+
+set -e # so the script stops if the build fails
+
+# Sends code to Google so it can build the image and save it.
+gcloud builds submit . --project "$PROJECT_ID" --tag "$IMAGE_URL"
+
+gcloud run deploy trips-backend \
+  --image="$IMAGE_URL" \
+  --region=$REGION \
+  --project=$PROJECT_ID
